c07d00d28e50dc3ff4ce5b2550b2c767f7cafe90
eaton-lab <de2356@columbia.edu>
2019-11-05 14:08:18 -0500

    version up

--- tetrad/__init__.py
+++ tetrad/__init__.py
@@ -3,14 +3,14 @@
 from .tetrad import Tetrad as tetrad
 
 # dunders mifflin
-__version__ = "0.9.10"
+__version__ = "0.9.11"
 __author__ = "Deren Eaton"
 __interactive__ = 1
 
 
 """
 TODO: 
-	- insert a test here to check MKL and OPENBLAS...
+    - insert a test here to check MKL and OPENBLAS...
     - clone vcf convert from ipyrad
     - add linkage option to CLI
     - speed up converter using numba
